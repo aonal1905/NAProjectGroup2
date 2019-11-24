898ff405206457ceba7db9bf6f866dcaf72665e5
aonal1905 <onalahmet86@gmail.com>
2019-11-24 17:07:37 -0600

    Campus life commit2

--- src/test/java/FeautureFiles/Admissions/campusLife.feature
+++ src/test/java/FeautureFiles/Admissions/campusLife.feature
@@ -1,3 +1,4 @@
+
 Feature: Campus Life Features
 
   Scenario: Search for student services under the campus life section and pop up menu of this section
@@ -7,3 +8,16 @@ Feature: Campus Life Features
     Then  The "student services" menu and pop up menu displayed
 
 
+  Scenario: Search for student organization under the student organization section and pop up menu of this section
+
+  Given user will navigate to the website
+  When user hover over mouse to Campus life
+  And I click on Student organization button
+  Then I should able to see the title
+
+  Scenario: Search for housing under the student organization section and pop up menu of this section
+
+    Given user will navigate to the website
+    When user hover over mouse to Campus life
+    And I click on housing button
+    Then I should able to see the handbook
--- src/test/java/PageObjectModel/HomePageCampusLife.java
+++ src/test/java/PageObjectModel/HomePageCampusLife.java
@@ -3,19 +3,39 @@ package PageObjectModel;
 import Common.Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
+import javax.swing.*;
 import java.sql.Driver;
 
 
 public class HomePageCampusLife extends Base {
   public   By search = By.xpath("//*[@id='sidebar']/ul/li[11]");
     public WebElement getSearchCamp(){
+
         return driver.findElement(search);
     }
+
     public   By searchStudentServices = By.className("page_item page-item-1323");
     public WebElement  getStudentServices() {
          return driver.findElement(searchStudentServices);
 
     }
+    public void hoveroverCampusLife(){
+        Actions ac= new Actions(driver);
+        ac.moveToElement(driver.findElement(By.xpath("//*[@id='menu-item-897']/a"))).build().perform();
+    }
+    public By clickStudentOrg= By.xpath("//*[@id='menu-item-5754']/a/span");
+    public WebElement getStudentOrg(){
+        return driver.findElement(clickStudentOrg);
+    }
+    public By clickHousing= By.xpath("//*[@id='menu-item-5758']/a/span");
+    public WebElement getHousing(){
+        return driver.findElement(clickHousing);
 
+    }
+    public By clickHandbook= By.xpath("//*[@id=\"post-1305\"]/div/div[2]/div/div[1]/div/div[1]/a");
+    public WebElement getHandBook(){
+        return driver.findElement(clickHandbook);
+    }
 }
