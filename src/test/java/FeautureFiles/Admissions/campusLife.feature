120530e85029569fb9835570a2fe799befdb6c4d
aonal1905 <onalahmet86@gmail.com>
2019-11-23 23:53:00 -0600

    Campus life commit

---
+++ src/test/java/FeautureFiles/Admissions/campusLife.feature
@@ -0,0 +1,9 @@
+Feature: Campus Life Features
+
+  Scenario: Search for student services under the campus life section and pop up menu of this section
+
+    Given user will navigate to the website
+    When user searches for student service section
+    Then  The "student services" menu and pop up menu displayed
+
+
--- src/test/java/PageObjectModel/HomePageCampusLife.java
+++ src/test/java/PageObjectModel/HomePageCampusLife.java
@@ -1,4 +1,21 @@
 package PageObjectModel;
 
-public class HomePageCampusLife {
+import Common.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.sql.Driver;
+
+
+public class HomePageCampusLife extends Base {
+  public   By search = By.xpath("//*[@id='sidebar']/ul/li[11]");
+    public WebElement getSearchCamp(){
+        return driver.findElement(search);
+    }
+    public   By searchStudentServices = By.className("page_item page-item-1323");
+    public WebElement  getStudentServices() {
+         return driver.findElement(searchStudentServices);
+
+    }
+
 }
